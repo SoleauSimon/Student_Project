847d1b67f3902749f55acf6ea74487ec44aa67f5
Simon <soleausimon@hotmail.fr>
2019-01-16 17:20:31 +0100

    Profile up

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,7 +1,7 @@
 import React, {Component, Fragment} from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Info from './Info.jsx';
-import Form from './Form.jsx';
+import Info from './StudentsInfos/Info.jsx';
+import Profile from './Profile/Profile'
 
 class App extends Component {
   render() {
@@ -12,7 +12,7 @@ class App extends Component {
           <Router>
             <Fragment>
               <Route exact path="/" component={Info}/>
-              {/* <Route path="/profile" component={}/> */}
+              <Route path="/profile" component={Profile}/>
             </Fragment>
           </Router>
         </div>
---
+++ imports/ui/Profile/Profile.jsx
@@ -0,0 +1,11 @@
+import React, {Component, Fragment} from 'react';
+
+class Profile extends Component {
+    render() {
+        return(
+            <div>Profile</div>
+        )
+    }
+}
+
+export default Profile
